595b39922800a22ce06a45cafbede499514b4710
Sir Valiance <sir@sirvaliance.com>
2011-06-11 01:34:57 -0500

    Created the basic structure for the tornado main.py app.  Created a basic main page template.

---
+++ app/main.py
@@ -0,0 +1,34 @@
+import os
+import logging
+import uuid
+
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+	
+
+	def get(self):
+		self.render("main_page.html")
+
+
+class Application(tornado.web.Application):
+
+
+	def __init__(self):
+		handlers = [
+			(r"/", MainHandler),
+		]
+
+		settings = dict(
+			template_path=os.path.join(os.path.dirname(__file__), "templates"),
+			static_path=os.path.join(os.path.dirname(__file__), "static"),
+		)
+		tornado.web.Application.__init__(self, handlers, **settings)
+
+
+if __name__ == "__main__":
+	application = Application()
+	application.listen(8888)
+	tornado.ioloop.IOLoop.instance().start()
