3c4e506fcb0eee95414bb1c797358c320e614a20
Tom Ritter <tom@ritter.vg>
2012-04-16 22:05:32 -0400

    giant reworking of the site for the relaunch.

--- app/main.py
+++ app/main.py
@@ -6,68 +6,21 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render("main_page.html")
+		self.render("main.html")
 class AboutHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("about.html")
-
-# Projects
-class ProjectsHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/projects.html")
-class IntroHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/intro.html")
 class AuditHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render("projects/audit.html")
-class BleedingHandler(tornado.web.RequestHandler):
+		self.render("cpr.html")
+class ProjectHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render("projects/bleeding.html")
-
-
-
-#CCH
-class CCHHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch.html")
-class AnonymizersHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch/anonymizers.html")
-class ChatHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch/chat.html")
-class CurrencyHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch/currency.html")
-class MobileHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch/mobile.html")
-class RemailersHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch/remailers.html")
-class StorageHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("projects/cch/storage.html")
-
-# Docs 
-class DocsHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("docs/docs.html")
-class ReadingHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("docs/reading.html")
-class ResearchHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("docs/research.html")
+		self.render("projects.html")
 
 # Interact 
 class InteractHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("interact/interact.html")
-class MailingListsHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("interact/mailing_lists.html")
 class InteractTimeHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render("interact/time.html")
@@ -83,26 +36,10 @@ class Application(tornado.web.Application):
 		handlers = [
 			(r"/", MainHandler),
 			(r"/about/", AboutHandler),
-
-			(r"/projects/", ProjectsHandler),
-			(r"/projects/intro/", IntroHandler),
-			(r"/projects/audit/", AuditHandler),
-			(r"/projects/bleeding/", BleedingHandler),
-			
-			(r"/projects/cch/", CCHHandler),
-			(r"/projects/cch/anonymizers/", AnonymizersHandler),
-			(r"/projects/cch/chat/", ChatHandler),
-			(r"/projects/cch/currency/", CurrencyHandler),
-			(r"/projects/cch/mobile/", MobileHandler),
-			(r"/projects/cch/remailers/", RemailersHandler),
-			(r"/projects/cch/storage/", StorageHandler),
-			
-			(r"/docs/", DocsHandler),
-			(r"/docs/reading/", ReadingHandler),
-			(r"/docs/research/", ResearchHandler),
+			(r"/projects/", ProjectHandler),
+            (r"/code-peer-review/", AuditHandler),
 			
 			(r"/interact/", InteractHandler),
-			(r"/interact/mailing_lists/", MailingListsHandler),
 			(r"/interact/time/", InteractTimeHandler),
 			(r"/interact/goods/", InteractGoodsHandler),
 			(r"/interact/money/", InteractMoneyHandler),
